ebac8b16cec87debccf56817f4bed5efcc219e8a
Anatoly <anatoly.gusakovsky@gmail.com>
2022-12-19 10:22:08 +0200

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+const title = document.createElement('h1');
+
+title.innerHTML = "tasks";
+
+document.body.append(title);
+const list = document.createElement('ul');
+
+const render = (arr) => {
+    // arr.map(el => {
+    //     const text
+    //     const asda
+    //     asdasd
+    //     asdas
+    // })
+}
