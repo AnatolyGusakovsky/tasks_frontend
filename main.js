b87337b713abe9110585aa01bac14a87b153aaa0
Anatoly <anatoly.gusakovsky@gmail.com>
2022-12-30 09:30:59 +0200

    added some improvements

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 let tasks_arr = [];
-let incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
+let incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
+let addTaskField = document.getElementById("add_task_field");
+let addButton=document.getElementById("submit_btn");//first button
 
-function add_task() {
-  let task_text = document.getElementById("task_field").value
+function addTask() {
+  let task_text = document.getElementById("add_task_field").value
 
-    let listItem=document.createElement("li");
-    let label=document.createElement("label");//label
-    let editInput=document.createElement("input");//text
-    let editButton=document.createElement("button");//edit button
-    let deleteButton=document.createElement("button");//delete button
+  let listItem = document.createElement("li");
+  let label = document.createElement("label");//label
+  let editInput = document.createElement("input");//text
+  let editButton = document.createElement("button");//edit button
+  let deleteButton = document.createElement("button");//delete button
 
-    label.innerText=task_text;
+  label.innerText = task_text;
 
-    editInput.type="text";
-    editButton.innerText="Edit";
-    editButton.className="edit";
-    deleteButton.innerText="Delete";
-    deleteButton.className="delete";
+  editInput.type = "text";
+  editButton.innerText = "Edit";
+  editButton.className = "edit";
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "delete";
 
-    listItem.appendChild(label);
-    listItem.appendChild(editInput);
-    listItem.appendChild(editButton);
-    listItem.appendChild(deleteButton);
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(editButton);
+  listItem.appendChild(deleteButton);
   incompleteTaskHolder.appendChild(listItem);
-    return listItem;
-
+  addTaskField.value = "";
+  return listItem;
 }
 
 
-
+//Set the click handler to the addTask function.
+// addButton.onclick = addTask;
+addButton.addEventListener("click", addTask);
