9b2683eb5bab55c490a593f5fe06c9bbed73ecf3
Anatoly <anatoly.gusakovsky@gmail.com>
2023-03-21 09:30:59 +0200

    In progress.

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,19 @@
-const http = require('http')
-const port = 3000;
-const fs = require ('fs')
+const Tasks_store = require ("../frontend/tasks_store.js");
+const Task = require ("../frontend/task.js");
 
-const server = http.createServer(function (req, res) {
+const http = require('http')
+const port = process.env.PORT;
+const fs = require ('fs')
+const task_store = new Tasks_store()
+// task_store.add_task(new Task('1', 'task text', false))
+
+const server = http.createServer(/*DO WE NEED ASYNC HERE?*/ function (req, res) {
+  if(req.url === 'api/todos' && req.method === "GET"){
+    res.writeHead(200, {"Content-type": "application/json"})
+    res.end(JSON.stringify(task_store.get_todo_tasks()))
+  }
+
+
   res.writeHead(200, {'Content-Type': 'text/html'})
   fs.readFile('index.html', function(error, data){
     if(error){
