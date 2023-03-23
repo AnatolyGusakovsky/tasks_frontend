f092e24c4086c946015ab4a3bc28e1a933f22daf
Anatoly <anatoly.gusakovsky@gmail.com>
2023-03-23 18:13:40 +0200

    API realisation started

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,17 @@
 import * as http from "http";
-import * as fs from "fs";
 import Tasks_store from "../frontend/tasks_store.js";
 import Task from "../frontend/task.js";
 
-const port = process.env.PORT; //todo: debug - port undefined
+const port = 3000;
 const task_store = new Tasks_store()
 task_store.add_task(new Task('1', 'task text', false))
 
 const server = http.createServer(/*DO WE NEED ASYNC HERE?*/ function (req, res) {
-  if (req.url === 'api/todos' && req.method === "GET") {
+  if (req.url === '/api/todos' && req.method === "GET") {
     res.writeHead(200, {"Content-type": "application/json"})
     res.end(JSON.stringify(task_store.get_todo_tasks()))
   }
 
-
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  fs.readFile('index.html', function (error, data) {
-    if (error) {
-      res.writeHead(404)
-      res.write('Error: File not found')
-    } else {
-      res.write(data)
-    }
-    res.end()
-  })
 })
 
 server.listen(port, function (error) {
